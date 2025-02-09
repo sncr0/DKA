e5085a0767aa586a530bbcc2ac039ed0e1eea188
Sam <samuel.moerman@gmail.com>
2025-02-08 19:56:48 -0500

    inter

--- src/app.py
+++ src/app.py
@@ -5,33 +5,55 @@ from enum import Enum
 from datetime import datetime
 from patient.patient import Patient, DKASeverity  # Import your Patient class
 
+
+def write_patient_data():
+    st.subheader("🏥 Patient Info:")
+    st.write(f"Patient ID: {st.session_state.patient_id}")
+    st.write(f"Patient Name: {st.session_state.patient.name}")
+    st.write(f"Patient Age: {st.session_state.patient.age}")
+    st.write(f"Patient Weight: {st.session_state.patient.weight} kg")
+    st.write(f"Patient Gender: {st.session_state.patient.gender}")
+
+
+def write_patient_measurements(measurement_dict):
+    st.subheader("📊 Patient Measurements:")
+    for key, value in measurement_dict.items():
+        st.write(f"{key}: {value}")
+
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
 st.title("🩺 DKA Patient Management")
 
-st.subheader("➕ Create a Patient")
 
 # Generate a unique patient ID
-if "patient_id" not in st.session_state:
+if "patient" not in st.session_state:
+    st.subheader("➕ Create a Patient")
+
     st.session_state.patient_id = str(uuid.uuid4())
 
-# Basic patient info
-name = st.text_input("Patient Name")
-age = st.number_input("Age", min_value=0, max_value=120, value=1)
-weight = st.number_input("Weight (kg)", min_value=0, value=1)
-gender = st.selectbox("Gender", ["Male", "Female", "Other"])
+    # Basic patient info
+    name = st.text_input("Patient Name")
+    age = st.number_input("Age", min_value=0, max_value=120, value=1)
+    weight = st.number_input("Weight (kg)", min_value=0, value=1)
+    gender = st.selectbox("Gender", ["Male", "Female", "Other"])
 
-if st.button("Create"):
-    if name:
-        # Store patient in session state
-        st.session_state.patient = Patient(
-            patient_id=st.session_state.patient_id, name=name, age=age, weight=weight, gender=gender
-        )
-        st.success(f"{name} added successfully!")
-    else:
-        st.error("❌ Please enter a name!")
+    if st.button("Create"):
+        if name:
+            # Store patient in session state
+            st.session_state.patient = Patient(
+                patient_id=st.session_state.patient_id, name=name, age=age, weight=weight, gender=gender
+            )
+            st.success(f"{name} added successfully!")
+            st.rerun()
+        else:
+            st.error("❌ Please enter a name!")
 
 # Only show admission fields if patient is created
-if "patient" in st.session_state:
-    st.subheader("🏥 Admit Patient")
+# if "patient" in st.session_state and len(st.session_state.history) == 0:
+if "patient" in st.session_state and len(st.session_state.history) == 0:
+    write_patient_data()
     patient = st.session_state.patient  # Retrieve stored patient
 
     sodium = st.number_input("Sodium (mmol/L)", min_value=100, max_value=170, value=140)
@@ -49,8 +71,31 @@ if "patient" in st.session_state:
         elif patient.admission_status == DKASeverity.MILD:
             st.write(f"✅ {patient.name} admitted: MILD")
 
+        st.session_state.history.append(
+            (
+                {
+                    "Time": datetime.now(),
+                    "Sodium": sodium,
+                    "Potassium": potassium,
+                    "Chloride": chloride,
+                    "Bicarbonate": bicarbonate,
+                    "pH": pH,
+                    "Glucose": glucose,
+                },
+                {
+                }
+            )
+        )
+        print(len(st.session_state.history))
+        st.rerun()
 
 # calculate anion gap
+if "patient" in st.session_state and len(st.session_state.history) > 0:
+    write_patient_data()
+    write_patient_measurements(st.session_state.history[0])
+
+    patient = st.session_state.patient
+
     if patient.admission_status:
         st.header("Patient Data")
 
@@ -64,13 +109,15 @@ if "patient" in st.session_state:
 
 
         st.header("Recommendations")
-
+        recommendations = []
         if anion_gap < 12:
             st.write("DKA Resolved")
+            recommendations.append("DKA Resolved")
             # resolution time = start - finish
         elif anion_gap > 12:
             if not patient.insulin_drip:
                 st.write("Start insulin drip at 0.1 units/kg/hr")
+                recommendations.append("Start insulin drip at 0.1 units/kg/hr")
                 patient.insulin_drip = True
 
         # GET ELECTROLYTES
@@ -84,28 +131,36 @@ if "patient" in st.session_state:
             if corrected_sodium > 135:
                 if potassium > 4:
                     st.write("Run IV fluids 0.45 NS @ 250 cc / hr")
+                    recommendations.append("Run IV fluids 0.45 NS @ 250 cc / hr")
                 else:
                     st.write("Run IV fluids 0.45 NS w 20 meq KCl @ 250 cc / hr")
+                    recommendations.append("Run IV fluids 0.45 NS w 20 meq KCl @ 250 cc / hr")
             elif corrected_sodium < 135:
                 if potassium > 4:
                     st.write("Run IV fluids NS @ 250 cc / hr")
+                    recommendations.append("Run IV fluids NS @ 250 cc / hr")
                 else:
                     st.write("Run IV fluids NS w 20 meq KCl @ 250 cc / hr")
+                    recommendations.append("Run IV fluids NS w 20 meq KCl @ 250 cc / hr")
         elif glucose < 250:
             if corrected_sodium > 135:
                 if potassium > 4:
                     st.write("Run IV fluids D5 0.45 NS @ 250 cc / hr")
+                    recommendations.append("Run IV fluids D5 0.45 NS @ 250 cc / hr")
                 else:
                     st.write("Run IV fluids D5 0.45 NS w 20 meq KCl @ 250 cc / hr")
+                    recommendations.append("Run IV fluids D5 0.45 NS w 20 meq KCl @ 250 cc / hr")
             elif corrected_sodium < 135:
                 if potassium > 4:
                     st.write("Run IV fluids D5 NS @ 250 cc / hr")
+                    recommendations.append("Run IV fluids D5 NS @ 250 cc / hr")
                 else:
                     st.write("Run IV fluids D5 NS w 20 meq KCl @ 250 cc / hr")
+                    recommendations.append("Run IV fluids D5 NS w 20 meq KCl @ 250 cc / hr")
 
         # measure_glucose()
         st.write("Come back in 1 hour with blood sugar reading")
-
+        recommendations.append("Come back in 1 hour with blood sugar reading")
 
         glucose = st.number_input("Glucose (mg/dL)", min_value=0, value=100, key=3)
         if st.button("Measure Glucose", key="{2}"):
